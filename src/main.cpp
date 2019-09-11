45f2b4555dcf336592749dfe13a1763e0b334851
Michal Gallus <michal.gallus@intel.com>
2019-09-11 13:42:39 +0200

    Add Math library with linking

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,8 @@
+#include "math.h"
+
 int main() {
+	float value = 2.0f;
+	float result = inverse(value);
 	return 0;
 }
+ 
---
+++ src/math.cpp
@@ -0,0 +1,5 @@
+#include "math.h"
+
+float inverse(float x) {
+    return 1.0f / x;
+}
