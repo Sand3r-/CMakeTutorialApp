490ea869cd38fbd8fcbfed0f69a91fe779d78a0f
Michal Gallus <michal.gallus@intel.com>
2019-10-13 14:07:34 +0200

    Download spdlog with ExternalProject

--- src/neuron/main.cpp
+++ src/neuron/main.cpp
@@ -1,6 +1,8 @@
+#include <spdlog/spdlog.h>
 #include "math_funcs.h"
 
 int main() {
+	spdlog::info("Hello world");
 	float value = 2.0f;
 	float result = inverse(value);
 	float* values = new float[20];
